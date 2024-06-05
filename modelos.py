d98450464d3f981801bbd80f035cba64f2288a30
carolina romero <99673763+caromerou@users.noreply.github.com>
2024-06-05 13:33:41 -0500

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -7,6 +7,22 @@ import streamlit as st
 
 
 
+from google.colab import files
+import pandas as pd
+
+# Subir el archivo
+uploaded = files.upload()
+
+# Listar los archivos subidos
+for filename in uploaded.keys():
+    print(f'Archivo subido: {filename}')
+
+# Leer el archivo con pandas
+# Supongamos que subiste un archivo Excel llamado 'mi_archivo.xlsx'
+df = pd.read_excel(filename)  # Usa pd.read_csv si es un archivo CSV
+
+# Mostrar las primeras filas del DataFrame
+print(df.head())
 
 
 
@@ -14,7 +30,7 @@ import streamlit as st
 
 
 # Leer el archivo CSV
-df = pd.read_csv('PS_20174392719_1491204439457_log.csv')
+#df = pd.read_csv('PS_20174392719_1491204439457_log.csv')
 
 # Codificar variables categóricas usando pandas
 df['type_encoded'] = df['type'].map({'CASH_IN': 0, 'CASH_OUT': 1, 'DEBIT': 2, 'PAYMENT': 3, 'TRANSFER': 4})
